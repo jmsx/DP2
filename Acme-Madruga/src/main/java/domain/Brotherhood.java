b2aa9c8012a81379d31ddd9079928b528ec26933
Alba Olmo <albaolmotrinidad@gmail.com>
2019-02-15 17:01:11 +0100

    Added enrollment and brotherhood entities

---
+++ Acme-Madruga/src/main/java/domain/Brotherhood.java
@@ -0,0 +1,54 @@
+
+package domain;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Brotherhood extends DomainEntity {
+
+	private String				title;
+	private Date				date;
+	private Collection<String>	pictures;
+
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@Past
+	@Temporal(TemporalType.DATE)
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(final Date date) {
+		this.date = date;
+	}
+
+	@URL
+	public Collection<String> getPictures() {
+		return this.pictures;
+	}
+
+	public void setPictures(final Collection<String> pictures) {
+		this.pictures = pictures;
+	}
+
+}
---
+++ Acme-Madruga/src/main/java/domain/Enrollment.java
@@ -0,0 +1,68 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Enrollment extends DomainEntity {
+
+	private Date		moment;
+	private Position	position;
+
+	//Relational atributes
+	private Member		member;
+	private Brotherhood	brotherhood;
+
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	@Valid
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(final Position position) {
+		this.position = position;
+	}
+
+	//Relational getters and setters
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Member getMember() {
+		return this.member;
+	}
+
+	public void setMember(final Member member) {
+		this.member = member;
+	}
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Brotherhood getBrotherhood() {
+		return this.brotherhood;
+	}
+
+	public void setBrotherhood(final Brotherhood brotherhood) {
+		this.brotherhood = brotherhood;
+	}
+
+}
