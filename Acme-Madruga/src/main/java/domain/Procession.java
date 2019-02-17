5d267060b90b2d8b4c0975d51d5aa57151ec0303
José Manuel González Gutiérrez <josgongut1@alum.us.es>
2019-02-17 12:34:31 +0100

    Java Domain and Populate

--- Acme-Madruga/src/main/java/domain/Procession.java
+++ Acme-Madruga/src/main/java/domain/Procession.java
@@ -1,12 +1,104 @@
 
 package domain;
 
+import java.util.Collection;
+import java.util.Date;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Procession extends DomainEntity {
+
+	private String				title;
+	private String				description;
+	private Date				moment;
+	private String				ticker;
+	private String				mode;
+
+	//Relational atributes
+	private Collection<Float>	floats;
+	private Brotherhood			brotherhood;
+
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@NotBlank
+	@SafeHtml
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+	@NotNull
+	@Future
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "^[0-9]{6}[A-Z]{5}$")
+	public String getTicker() {
+		return this.ticker;
+	}
+
+	public void setTicker(final String ticker) {
+		this.ticker = ticker;
+	}
+	@Pattern(regexp = "^(DRAFT|FINAL)$")
+	public String getMode() {
+		return this.mode;
+	}
+
+	public void setMode(final String mode) {
+		this.mode = mode;
+	}
+
+	//Relational Methods
+	@Valid
+	@ManyToMany
+	public Collection<Float> getFloats() {
+		return this.floats;
+	}
+
+	public void setFloats(final Collection<Float> floats) {
+		this.floats = floats;
+	}
+	@Valid
+	@ManyToOne(optional = false)
+	public Brotherhood getBrotherhood() {
+		return this.brotherhood;
+	}
+
+	public void setBrotherhood(final Brotherhood brotherhood) {
+		this.brotherhood = brotherhood;
+	}
 
 }
--- Acme-Madruga/src/main/java/domain/Request.java
+++ Acme-Madruga/src/main/java/domain/Request.java
@@ -1,12 +1,98 @@
 
 package domain;
 
+import java.util.Date;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.SafeHtml;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Request extends DomainEntity {
+
+	private String		status;
+	private Date		moment;
+	private Integer		row;
+	private Integer		column;
+	private String		explanation;
+
+	//Relation attributes
+	private Member		member;
+	private Procession	procession;
+
+
+	@Pattern(regexp = "^(APPROVED|PENDING|REJECTED)$")
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(final String status) {
+		this.status = status;
+	}
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+	@Min(1)
+	public Integer getRow() {
+		return this.row;
+	}
+
+	public void setRow(final Integer row) {
+		this.row = row;
+	}
+	@Min(1)
+	public Integer getColumn() {
+		return this.column;
+	}
+
+	public void setColumn(final Integer column) {
+		this.column = column;
+	}
+	@SafeHtml
+	public String getExplanation() {
+		return this.explanation;
+	}
+
+	public void setExplanation(final String explanation) {
+		this.explanation = explanation;
+	}
+
+	//Relation metods
+	@Valid
+	@ManyToOne(optional = false)
+	public Member getMember() {
+		return this.member;
+	}
+
+	public void setMember(final Member member) {
+		this.member = member;
+	}
+	@Valid
+	@ManyToOne(optional = false)
+	public Procession getProcession() {
+		return this.procession;
+	}
+
+	public void setProcession(final Procession procession) {
+		this.procession = procession;
+	}
 
 }
