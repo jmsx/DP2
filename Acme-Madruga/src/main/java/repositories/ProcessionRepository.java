aa2e4f42011ce1d97a303307b750129db927a031
Jesús Manuel García Lanzas <jesgarlan@alum.us.es>
2019-02-19 20:04:31 +0100

    Procession repo y Service (Faltan save y delete).

--- Acme-Madruga/src/main/java/repositories/ProcessionRepository.java
+++ Acme-Madruga/src/main/java/repositories/ProcessionRepository.java
@@ -1,12 +1,24 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Procession;
 
 @Repository
 public interface ProcessionRepository extends JpaRepository<Procession, Integer> {
+
+	@Query("select p from Procession p where p.brotherhood.userAccount.id=?1")
+	Collection<Procession> findAllProcessionByBrotherhoodId(Integer id);
+
+	@Query("select p from Request r join r.Procession p where r.member.userAccount.id=?1")
+	Collection<Procession> findAllProcessionByBMemberId(Integer id);
+
+	@Query("select p from Procession p where p.ticker = ?1")
+	Collection<Procession> getProcessionWithTicker(String ticker);
 
 }
--- Acme-Madruga/src/main/java/services/ProcessionService.java
+++ Acme-Madruga/src/main/java/services/ProcessionService.java
@@ -1,20 +1,24 @@
 
 package services;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.ProcessionRepository;
+import security.Authority;
 import domain.Actor;
 import domain.Brotherhood;
 import domain.FloatProcession;
-import domain.Folder;
-import domain.Message;
 import domain.Procession;
 
 @Service
@@ -26,6 +30,9 @@ public class ProcessionService {
 
 	@Autowired
 	private BrotherhoodService		brotherhoodService;
+
+	@Autowired
+	private ActorService			actorService;
 
 
 	public Procession create() {
@@ -42,152 +49,100 @@ public class ProcessionService {
 	}
 
 	public Collection<Procession> findAll() {
-		final Collection<Procession> res = this.processionRepository.findAll();
-		Assert.notNull(res);
+		Collection<Procession> res = new ArrayList<>();
+		final Actor principal = this.actorService.findByPrincipal();
+		final Boolean isBrotherhood = this.actorService.checkAuthority(principal, Authority.BROTHERHOOD);
+		final Boolean isMember = this.actorService.checkAuthority(principal, Authority.MEMBER);
 
+		if (isBrotherhood)
+			res = this.processionRepository.findAllProcessionByBrotherhoodId(principal.getUserAccount().getId());
+		else if (isMember)
+			res = this.processionRepository.findAllProcessionByBMemberId(principal.getUserAccount().getId());
+		//Si salta puede ser un Admin
+		Assert.notNull(res);
 		return res;
 	}
 
 	public Procession findOne(final int processionId) {
+		Assert.isTrue(processionId != 0);
 		final Procession res = this.processionRepository.findOne(processionId);
-
+		Assert.notNull(res);
 		return res;
 	}
 
-	public Folder save(final Folder f, final Actor a) {
-		Assert.notNull(f);
-		Assert.notNull(a);
+	//	public Folder save(final Folder f, final Actor a) {
+	//		Assert.notNull(f);
+	//		Assert.notNull(a);
+	//
+	//		Folder saved;
+	//		final boolean bool = this.checkForSpamWords(f);
+	//
+	//		if (bool)
+	//			a.setSpammer(true);
+	//
+	//		if (f.getId() == 0)
+	//			saved = this.folderRepository.save(f);
+	//		else {
+	//			final Collection<Folder> fs = this.findAllByUserId(a.getUserAccount().getId());
+	//			Assert.isTrue(fs.contains(f));
+	//			saved = this.folderRepository.save(f);
+	//		}
+	//		return saved;
+	//	}
+	//
+	//	public void delete(final Folder f) {
+	//		Assert.notNull(f);
+	//		Assert.isTrue(!f.getIsSystemFolder());
+	//		Assert.isTrue(f.getId() != 0);
+	//
+	//		final Actor principal = this.actorService.findByPrincipal();
+	//		final Collection<Folder> fs = this.findAllByUserId(principal.getUserAccount().getId());
+	//		final Collection<Message> ms = this.messageService.findAllByFolderIdAndUserId(f.getId(), principal.getUserAccount().getId());
+	//		Assert.isTrue(fs.contains(f));
+	//
+	//		if (!ms.isEmpty())
+	//			//El delete de folder falla por este clear, ya que se le pasa al metodo deleteAll() una carpeta vacia y falla el assert de que no este vacia
+	//			//f.getMessages().clear();
+	//			this.messageService.deleteAll(ms, f);
+	//
+	//		this.folderRepository.delete(f);
+	//		this.actorService.update(principal);
+	//	}
 
-		Folder saved;
-		final boolean bool = this.checkForSpamWords(f);
+	/* ========================= OTHER METHODS =========================== */
 
-		if (bool)
-			a.setSpammer(true);
+	private String generateTicker(final Date date) {
+		String res = "";
+		final SimpleDateFormat myFormat = new SimpleDateFormat("yyMMdd", Locale.ENGLISH);
+		final String YYMMMDDD = myFormat.format(date);
+		final String word = RandomStringUtils.randomAlphabetic(6);
+		final int max = 99;
+		final int min = 10;
+		final Random rand = new Random();
+		final int randomNum = rand.nextInt((max - min) + 1) + min;
+		final String num = String.valueOf(randomNum);
+		final String yy = YYMMMDDD.substring(0, 2);
+		final String mm = YYMMMDDD.substring(2, 5);
+		final String dd = YYMMMDDD.substring(5, 6);
+		final String tickr = yy + mm + dd + '-' + word;
+		res = tickr;
 
-		if (f.getId() == 0)
-			saved = this.folderRepository.save(f);
-		else {
-			final Collection<Folder> fs = this.findAllByUserId(a.getUserAccount().getId());
-			Assert.isTrue(fs.contains(f));
-			saved = this.folderRepository.save(f);
+		if (this.hasDuplicate(res))
+			this.generateTicker(date);
+		return res;
+	}
+
+	private boolean hasDuplicate(final String ticker) {
+
+		boolean res = true;
+		try {
+			if (this.processionRepository.getProcessionWithTicker(ticker).isEmpty())
+				res = false;
+		} catch (final Exception e) {
+			e.printStackTrace();
 		}
-		return saved;
-	}
 
-	private boolean checkForSpamWords(final Folder f) {
-		final Collection<String> words = new ArrayList<>();
-		words.add(f.getName());
-
-		return this.configurationParametersService.checkForSpamWords(words);
-	}
-
-	public void delete(final Folder f) {
-		Assert.notNull(f);
-		Assert.isTrue(!f.getIsSystemFolder());
-		Assert.isTrue(f.getId() != 0);
-
-		final Actor principal = this.actorService.findByPrincipal();
-		final Collection<Folder> fs = this.findAllByUserId(principal.getUserAccount().getId());
-		final Collection<Message> ms = this.messageService.findAllByFolderIdAndUserId(f.getId(), principal.getUserAccount().getId());
-		Assert.isTrue(fs.contains(f));
-
-		if (!ms.isEmpty())
-			//El delete de folder falla por este clear, ya que se le pasa al metodo deleteAll() una carpeta vacia y falla el assert de que no este vacia
-			//f.getMessages().clear();
-			this.messageService.deleteAll(ms, f);
-
-		this.folderRepository.delete(f);
-		this.actorService.update(principal);
-	}
-
-	public Collection<Folder> setFoldersByDefault(final Actor actor) {
-		final Collection<Folder> folders = new ArrayList<Folder>();
-		final Collection<Message> messages = new ArrayList<Message>();
-
-		final Folder inbox = this.create();
-		inbox.setName("In box");
-		inbox.setIsSystemFolder(true);
-		inbox.setActor(actor);
-		inbox.setMessages(messages);
-		folders.add(inbox);
-
-		final Folder outbox = this.create();
-		outbox.setName("Out box");
-		outbox.setIsSystemFolder(true);
-		outbox.setActor(actor);
-		outbox.setMessages(messages);
-		folders.add(outbox);
-
-		final Folder trash = this.create();
-		trash.setName("Trash box");
-		trash.setIsSystemFolder(true);
-		trash.setActor(actor);
-		trash.setMessages(messages);
-		folders.add(trash);
-
-		final Folder spam = this.create();
-		spam.setName("Spam box");
-		spam.setIsSystemFolder(true);
-		spam.setActor(actor);
-		spam.setMessages(messages);
-		folders.add(spam);
-
-		final Folder notification = this.create();
-		notification.setName("Notification box");
-		notification.setIsSystemFolder(true);
-		notification.setActor(actor);
-		notification.setMessages(messages);
-		folders.add(notification);
-
-		return this.saveAll(folders);
-	}
-
-	public Collection<Folder> saveAll(final Collection<Folder> fs) {
-		Assert.notEmpty(fs);
-		return this.folderRepository.save(fs);
-	}
-
-	public Collection<Folder> findAllByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findAllByUserId(id);
-	}
-
-	public Folder findInboxByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findInboxByUserId(id);
-	}
-
-	public Folder findOutboxByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findOutboxByUserId(id);
-	}
-
-	public Folder findSpamboxByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findSpamboxByUserId(id);
-	}
-
-	public Folder findTrashboxByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findTrashboxByUserId(id);
-	}
-
-	public Folder findNotificationboxByUserId(final int id) {
-		Assert.isTrue(id != 0);
-
-		return this.folderRepository.findNotificationboxByUserId(id);
-	}
-
-	public Collection<Folder> findAllByMessageIdAndUserId(final int mid, final int uid) {
-		Assert.isTrue(mid != 0);
-		Assert.isTrue(uid != 0);
-
-		return this.folderRepository.findAllByMessageIdAndUserId(mid, uid);
+		return res;
 	}
 
 }
