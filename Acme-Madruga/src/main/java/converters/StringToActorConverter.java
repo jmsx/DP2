2c8c5cd2f15adf89393854302217a84580311754
Alba Olmo <albaolmotrinidad@gmail.com>
2019-02-20 13:39:24 +0100

    Auto stash before merge of "master" and "origin/master"
    StringToSocialProfileConverter and StringToActorConverter updated

--- Acme-Madruga/src/main/java/converters/StringToActorConverter.java
+++ Acme-Madruga/src/main/java/converters/StringToActorConverter.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import repositories.AdministratorRepository;
+import repositories.BrotherhoodRepository;
+import repositories.MemberRepository;
 import domain.Actor;
 
 @Component
@@ -35,18 +37,16 @@ public class StringToActorConverter implements Converter<String, Actor> {
 			else {
 				id = Integer.valueOf(text);
 				result = this.brotherhoodRepository.findOne(id);
-					if (result == null) {
-						result = this.memberRepository.findOne(id);
-							if (result == null)
-								result = this.administratorRepository.findOne(id);
-						}
-					}
+				if (result == null) {
+					result = this.memberRepository.findOne(id);
+					if (result == null)
+						result = this.administratorRepository.findOne(id);
 				}
 
 			}
 
 		} catch (final Throwable oops) {
-			throw new IllegalArgumentException(this.oops);
+			throw new IllegalArgumentException(oops);
 		}
 		return result;
 	}
