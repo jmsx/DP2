fbaffa259bd998a2518c38b02ea662ada77c584e
Jesús Manuel García Lanzas <jesgarlan@alum.us.es>
2019-03-01 18:50:13 +0100

    Edit de Enrolment corregido

--- Acme-Madruga/src/main/java/services/BrotherhoodService.java
+++ Acme-Madruga/src/main/java/services/BrotherhoodService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import org.springframework.validation.BindingResult;
 
 import repositories.BrotherhoodRepository;
 import security.Authority;
@@ -18,32 +17,22 @@ import security.UserAccount;
 import domain.Actor;
 import domain.Area;
 import domain.Brotherhood;
-import forms.ActorFrom;
 
 @Service
 @Transactional
 public class BrotherhoodService {
 
 	@Autowired
-	private BrotherhoodRepository						brotherhoodRepository;
+	private BrotherhoodRepository	brotherhoodRepository;
 
 	@Autowired
-	private ActorService								actorService;
+	private ActorService			actorService;
 
 	@Autowired
-	private FolderService								folderService;
+	private FolderService			folderService;
 
 	@Autowired
-	private MemberService								memberService;
-
-	@Autowired
-	private UserAccountService							userAccountService;
-
-	@Autowired
-	private org.springframework.validation.Validator	validator;
-
-	@Autowired
-	private EnrolmentService							enrolmentService;
+	private EnrolmentService		enrolmentService;
 
 
 	public Brotherhood create() {
@@ -127,62 +116,6 @@ public class BrotherhoodService {
 		Assert.isTrue(principal.getArea() == null);
 		principal.setArea(area);
 		this.save(principal);
-	}
-
-	//	public void registerAsBrotherhood(Brotherhood brotherhood) {
-	//		brotherhood = this.create();
-	//		final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
-	//		final String hash = encoder.encodePassword(brotherhood.getUserAccount().getPassword(), null);
-	//		brotherhood.getUserAccount().setPassword(hash);
-	//		brotherhood.setSpammer(false);
-	//		brotherhood.setScore(0.0);
-	//		brotherhood.setPictures(new ArrayList<String>());
-	//		brotherhood.setDate(new Date(System.currentTimeMillis() - 1));
-	//
-	//		final Brotherhood saved = this.brotherhoodRepository.save(brotherhood);
-	//		this.folderService.setFoldersByDefault(saved);
-	//	}
-
-	public Brotherhood reconstruct(final ActorFrom actorForm, BindingResult binding) {
-		binding = binding;
-		Brotherhood brotherhood;
-		if (actorForm.getId() == 0) {
-			brotherhood = this.create();
-			brotherhood.setName(actorForm.getName());
-			brotherhood.setMiddleName(actorForm.getMiddleName());
-			brotherhood.setSurname(actorForm.getSurname());
-			brotherhood.setPhoto(actorForm.getPhoto());
-			brotherhood.setPhone(actorForm.getPhone());
-			brotherhood.setEmail(actorForm.getEmail());
-			brotherhood.setAddress(actorForm.getAddress());
-			brotherhood.setScore(0.0);
-			brotherhood.setSpammer(false);
-			final UserAccount account = this.userAccountService.create();
-			final Collection<Authority> authorities = new ArrayList<>();
-			final Authority auth = new Authority();
-			auth.setAuthority(Authority.BROTHERHOOD);
-			authorities.add(auth);
-			account.setAuthorities(authorities);
-			account.setUsername(actorForm.getUserAccountuser());
-			account.setPassword(actorForm.getUserAccountpassword());
-			brotherhood.setUserAccount(account);
-		} else {
-			brotherhood = this.brotherhoodRepository.findOne(actorForm.getId());
-			brotherhood.setName(actorForm.getName());
-			brotherhood.setMiddleName(actorForm.getMiddleName());
-			brotherhood.setSurname(actorForm.getSurname());
-			brotherhood.setPhoto(actorForm.getPhoto());
-			brotherhood.setPhone(actorForm.getPhone());
-			brotherhood.setEmail(actorForm.getEmail());
-			brotherhood.setAddress(actorForm.getAddress());
-			final UserAccount account = this.userAccountService.findOne(brotherhood.getUserAccount().getId());
-			account.setUsername(actorForm.getUserAccountuser());
-			account.setPassword(actorForm.getUserAccountpassword());
-			brotherhood.setUserAccount(account);
-		}
-		this.validator.validate(brotherhood.getUserAccount(), binding);
-		this.validator.validate(brotherhood, binding);
-		return brotherhood;
 	}
 
 	public Collection<Brotherhood> AllBrotherhoodsFree() {
