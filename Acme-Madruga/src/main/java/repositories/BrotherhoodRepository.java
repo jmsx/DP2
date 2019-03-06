68363cf4acd21e08c033b3c912df95bbec7d032a
albaolmo <albaolmotrinidad@gmail.com>
2019-03-06 15:16:24 +0100

    queries updated

--- Acme-Madruga/src/main/java/repositories/BrotherhoodRepository.java
+++ Acme-Madruga/src/main/java/repositories/BrotherhoodRepository.java
@@ -19,18 +19,18 @@ public interface BrotherhoodRepository extends JpaRepository<Brotherhood, Intege
 	@Query("select count(e) from Enrolment e where e.dropOut=null group by e.member")
 	Collection<Integer> getNumberOfMembersPerBrotherhood();
 
-	@Query(value = "SELECT STDDEV(x),MAX(x),MIN(x),AVG(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT GROUP BY brotherhood) AS x", nativeQuery = true)
+	@Query(value = "SELECT STDDEV(x),MAX(x),MIN(x),AVG(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT WHERE ENROLMENT.drop_out IS NULL GROUP BY brotherhood) AS x", nativeQuery = true)
 	Double[] getStatisticsOfMembersPerBrotherhood();
 
 	/** The largest brotherhood is the one with highest number of members **/
 	@Query(
-		value = "SELECT ENROLMENT.brotherhood FROM `acme-madruga`.ENROLMENT LEFT OUTER JOIN BROTHERHOOD ON ENROLMENT.id=BROTHERHOOD.id GROUP BY brotherhood HAVING COUNT(*) = (SELECT MAX(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT GROUP BY brotherhood)AS T)",
+		value = "SELECT ENROLMENT.brotherhood FROM `acme-madruga`.ENROLMENT LEFT OUTER JOIN BROTHERHOOD ON ENROLMENT.id=BROTHERHOOD.id WHERE ENROLMENT.drop_out IS NULL GROUP BY brotherhood HAVING COUNT(*) = (SELECT MAX(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT GROUP BY brotherhood)AS T)",
 		nativeQuery = true)
 	Integer[] getLargestBrotherhood();
 
 	/** The smallest brotherhood is the one with lowest number of members **/
 	@Query(
-		value = "SELECT ENROLMENT.brotherhood FROM `acme-madruga`.ENROLMENT LEFT OUTER JOIN BROTHERHOOD ON ENROLMENT.id=BROTHERHOOD.id GROUP BY brotherhood HAVING COUNT(*) = (SELECT MIN(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT GROUP BY brotherhood)AS T)",
+		value = "SELECT ENROLMENT.brotherhood FROM `acme-madruga`.ENROLMENT LEFT OUTER JOIN BROTHERHOOD ON ENROLMENT.id=BROTHERHOOD.id WHERE ENROLMENT.drop_out IS NULL GROUP BY brotherhood HAVING COUNT(*) = (SELECT MIN(x) FROM (SELECT COUNT(*) AS x FROM `acme-madruga`.ENROLMENT GROUP BY brotherhood)AS T)",
 		nativeQuery = true)
 	Integer[] getSmallestBrotherhood();
 
