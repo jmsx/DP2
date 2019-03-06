456a5cc18310adf948010ac10dcd89c9d9ef0973
albaolmo <albaolmotrinidad@gmail.com>
2019-03-06 16:55:49 +0100

    welcome message + phone pttern

--- Acme-Madruga/src/main/java/controllers/WelcomeController.java
+++ Acme-Madruga/src/main/java/controllers/WelcomeController.java
@@ -14,19 +14,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.ActorService;
+import services.ConfigurationParametersService;
 
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
 
 	@Autowired
-	private ActorService	actorService;
+	private ActorService					actorService;
+
+	@Autowired
+	private ConfigurationParametersService	configurationParametersService;
 
 
 	// Constructors -----------------------------------------------------------
@@ -43,12 +48,21 @@ public class WelcomeController extends AbstractController {
 		SimpleDateFormat formatter;
 		String moment;
 
+		final String lang = LocaleContextHolder.getLocale().getLanguage();
+
+		String mensaje = null;
+		if (lang.equals("en"))
+			mensaje = this.configurationParametersService.findWelcomeMessageEn();
+		else if (lang.equals("es"))
+			mensaje = this.configurationParametersService.findWelcomeMessageEsp();
+
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
 
 		result = new ModelAndView("welcome/index");
 		result.addObject("name", name);
 		result.addObject("moment", moment);
+		result.addObject("mensaje", mensaje);
 
 		return result;
 	}
