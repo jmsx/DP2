d72d27b695d560eb2787f083aee9bf834b82b044
a8081 <amartinezr98@protonmail.com>
2019-03-07 09:54:33 +0100

    Resolving server issue

--- Acme-Madruga/src/main/java/services/FinderService.java
+++ Acme-Madruga/src/main/java/services/FinderService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import org.springframework.validation.BindingResult;
 
 import repositories.FinderRepository;
 import domain.Finder;
@@ -71,14 +70,7 @@ public class FinderService {
 	// Antes de guardar tengo que pasar por este metodo para setearle las nuevas procesiones segun los nuevos parametros
 	public Finder find(final Finder finder) {
 		this.memberService.findByPrincipal();
-		// final Collection<Procession> finalMode = this.processionService.findAllFinalMode();
-		final Collection<Procession> result;//= new ArrayList<Procession>();
-		//		for (final Procession p : finalMode)
-		//			if ((finder.getKeyword() == "" || (p.getDescription().contains(finder.getKeyword()) || p.getTitle().contains(finder.getKeyword()) || p.getTicker().contains(finder.getKeyword())
-		//				&& (finder.getAreaName() == "" || p.getBrotherhood().getArea().getName().contains(finder.getAreaName())) && (finder.getMaxDate() == null || p.getMoment().before(finder.getMaxDate()))
-		//				&& (finder.getMinDate() == null || p.getMoment().after(finder.getMinDate())))))
-		//				result.add(p);
-		result = this.finderRepository.findProcessions(finder.getKeyword(), finder.getMinDate(), finder.getMaxDate(), finder.getAreaName());
+		final Collection<Procession> result = this.finderRepository.findProcessions(finder.getKeyword(), finder.getMinDate(), finder.getMaxDate(), finder.getAreaName());
 		finder.setProcessions(result);
 		return this.save(finder);
 	}
@@ -89,13 +81,8 @@ public class FinderService {
 		Assert.isTrue(finder.getId() != 0);
 		Assert.isTrue(this.finderRepository.findMemberFinder(member.getId()).getId() == finder.getId(), "You're not owner of this finder, you cannot modify it");
 
-		//		if (finder.getMinDate() == null)
-		//			finder.setMinDate(new Date());
-		//		if (finder.getMaxDate() == null)
-		//			finder.setMaxDate(new Date());
 		finder.setCreationDate(new Date());
 		final Finder res = this.finderRepository.save(finder);
-		//Assert.notNull(me);
 		Assert.notNull(res);
 
 		member.setFinder(finder);
@@ -182,27 +169,6 @@ public class FinderService {
 	public Double getRatioEmptyFinders() {
 		final Double result = this.finderRepository.getRatioEmptyFinders();
 		Assert.notNull(result);
-		return result;
-	}
-
-	// TODO
-	public Finder reconstruct(final Finder finder, final BindingResult binding) {
-		Finder result;
-
-		result = this.finderRepository.findOne(finder.getId());
-		Assert.notNull(result);
-
-		final Finder finderClonado = (Finder) result.clone();
-
-		finderClonado.setKeyword(finder.getKeyword());
-		finderClonado.setAreaName(finder.getAreaName());
-		finderClonado.setMinDate(finder.getMinDate());
-		finderClonado.setMaxDate(finder.getMaxDate());
-
-		result = finderClonado;
-
-		this.validator.validate(result, binding);
-
 		return result;
 	}
 }
